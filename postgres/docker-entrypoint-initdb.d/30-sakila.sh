8834910d5313431425a64bdd5433bd3b6558fe6b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2021-03-09 19:35:57 +0100

    New link sakila db

--- postgres/docker-entrypoint-initdb.d/30-sakila.sh
+++ postgres/docker-entrypoint-initdb.d/30-sakila.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" <<-EOSQL
      CREATE DATABASE sakila;
 EOSQL
 
-wget https://s3.amazonaws.com/assets.datacamp.com/course/sql/dvdrental.zip; unzip dvdrental.zip
+cd /tmp
+wget https://sp.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip; unzip dvdrental.zip
 pg_restore -U postgres -d sakila dvdrental.tar
