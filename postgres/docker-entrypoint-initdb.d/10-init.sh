5555e81fcdd1e013aca118d18123073952f36bf2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2021-03-09 18:54:07 +0100

    Update 10-init.sh

--- postgres/docker-entrypoint-initdb.d/10-init.sh
+++ postgres/docker-entrypoint-initdb.d/10-init.sh
@@ -4,9 +4,6 @@ set -e
 # Override settings
 echo "include_dir = '/conf.d'" >> "$PGDATA/postgresql.conf"
 
-# Create pg_hba entry for replication
-echo "host replication $REPLICATION_USER 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
-
 # Create users
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
      CREATE EXTENSION pg_stat_statements;
